7b4604a3415f0111d004b2171bda1d68c012783b
Jijun Li <season.chopsticks@gmail.com>
2014-02-10 20:58:38 +0800

    modify a bug in navigation if eav exists

--- m.mobvoi/template/eav_one.js
+++ m.mobvoi/template/eav_one.js
@@ -2,7 +2,7 @@
 	var script = document.createElement("script");
 	script.setAttribute("type", "text/html");
 	script.setAttribute("id", "wrapper_eav_one");
-	script.innerHTML = '<ul class="cards"> <li class="cards_card"> <div class="cards_card_entire"> <p><span class="layout-orange">{ body[0].children[0].title }</span></p> <p><span class="layout-12px layout-666">{ body[0].children[0].content[0] }</span></p> <p><span class="layout-12px layout-666">{ body[0].children[0].content[1] }</span></p> </div> </li> </ul>';
+	script.innerHTML = '{ if header.task != "public.navigation" }<ul class="cards"> <li class="cards_card"> <div class="cards_card_entire"> <p><span class="layout-orange">{ body[0].children[0].title }</span></p> <p><span class="layout-12px layout-666">{ body[0].children[0].content[0] }</span></p> <p><span class="layout-12px layout-666">{ body[0].children[0].content[1] }</span></p> </div> </li> </ul>{ /if }';
 	document.head.appendChild(script);
 }) ()
 
