17a580dd44299500034572048c0fcf4e38fb8efa
Jijun Li <season.chopsticks@gmail.com>
2014-02-14 17:15:32 +0800

    feature: auto define weiyuyi blog ga label depending on the blog title

--- weiyuyi/oss/assets/js/weiyuyi.blog.js
+++ weiyuyi/oss/assets/js/weiyuyi.blog.js
@@ -1,4 +1,5 @@
-$("label:eq(1)").append(" <b>留空则不追踪访问数据</b>");
+$("#label").prev().hide();
+$("#label").hide();
 
 if(window.location.href.indexOf("edit") > 0) {
 	$.ajax({
@@ -21,24 +22,31 @@ if(window.location.href.indexOf("new") > 0) {
 	$("[name='blog[wechat_app_id]']").val(url.id)
 }
 
+$('[name="blog[title]"]').keyup(function() {
+	$("#label").val($('[name="blog[title]"]').val());
+})
+$('[name="blog[title]"]').blur(function() {
+	$("#label").val($('[name="blog[title]"]').val());
+})
+
 $("form").submit(function() {
 	if($.trim($("[name='blog[title]']").val()) == "") {
 		alert("error: lose something");
 		return false;
 	} else {
 		if($.trim($("#label").val()) != "") {
-		var a = '<script src="http://mobvoi-one-box.oss.aliyuncs.com/js/detectiveBrowser.2.0.js"></script>';
-			a += '<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");';
-			a += 'ga("create", "UA-46226479-2", "aliyuncs.com");';
-			a += 'ga("send", "pageview", {title: \'' + $("#label").val() + '\'});</script>';
-			a += $("[name='blog[content]']").val();
-			a += '<script>$("a").click(function(){if($(this).attr("href").indexOf("{download}") > 0){';
-			a += 'var b=$(this).attr("href").split("{download}")[0];';
-			a += 'if(browser.browser=="wechat"){setTimeout("window.location.href=\'http://mobvoi-one-box.oss.aliyuncs.com/weiyuyi/production/blogs/android.html?url=" + b + "&title=' + $("#label").val() + '&download=instance\'", 500);}';
-			a += 'else{ga("send", "event", "download", "' + $("#label").val() + '", browser.type + "/" + browser.os + "/" + browser.browser);setTimeout("window.location.href = \'" + b + "\'", 500);}return false;}});';
-			a += 'var CMWW = new chumenwenwen();CMWW.share.init({title:$("title").html(), desc: $("title").html(), img: $("img")[0].src, url: window.location.href});CMWW.share.wechatReady();</script>';
-			$("[name='blog[content]']").val(a);
-			return true;
+			var a = '<script src="http://mobvoi-one-box.oss.aliyuncs.com/js/detectiveBrowser.2.0.js"></script>';
+				a += '<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");';
+				a += 'ga("create", "UA-46226479-2", "aliyuncs.com");';
+				a += 'ga("send", "pageview", {title: \'' + $("#label").val() + '\'});</script>';
+				a += $("[name='blog[content]']").val();
+				a += '<script>$("a").click(function(){if($(this).attr("href").indexOf("{download}") > 0){';
+				a += 'var b=$(this).attr("href").split("{download}")[0];';
+				a += 'if(browser.browser=="wechat"){setTimeout("window.location.href=\'http://mobvoi-one-box.oss.aliyuncs.com/weiyuyi/production/blogs/android.html?url=" + b + "&title=' + $("#label").val() + '&download=instance\'", 500);}';
+				a += 'else{ga("send", "event", "download", "' + $("#label").val() + '", browser.type + "/" + browser.os + "/" + browser.browser);setTimeout("window.location.href = \'" + b + "\'", 500);}return false;}});';
+				a += 'var CMWW = new chumenwenwen();CMWW.share.init({title:$("title").html(), desc: $("title").html(), img: $("img")[0].src, url: window.location.href});CMWW.share.wechatReady();</script>';
+				$("[name='blog[content]']").val(a);
+				return true;
 		}
 	}
 })
